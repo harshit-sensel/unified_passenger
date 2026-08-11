e87902b28e62662e355cd6d0abfde43573c04e8c
Harshit Sensel <harshit@sensel.in>
2026-08-11 18:20:39 +0530

    fix(syntax): restore onOptionsItemSelected method signature in TagIn.java

--- Passenger_uni/app/src/main/java/com/sensel/passengerpro/TagIn.java
+++ Passenger_uni/app/src/main/java/com/sensel/passengerpro/TagIn.java
@@ -1027,6 +1027,11 @@ public class TagIn extends AppCompatActivity {
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 
