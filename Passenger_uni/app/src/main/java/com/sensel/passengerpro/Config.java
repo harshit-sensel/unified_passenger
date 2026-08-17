897374f38d86726e71b55ea5c3df25b6c2d0e9ba
Harshit Sensel <harshit@sensel.in>
2026-08-17 15:05:38 +0530

    Remove dead legacy WCF upload URLs from Config.java

--- Passenger_uni/app/src/main/java/com/sensel/passengerpro/Config.java
+++ Passenger_uni/app/src/main/java/com/sensel/passengerpro/Config.java
@@ -1,21 +1,13 @@
 package com.sensel.passengerpro;
 
-
-
-import java.security.GeneralSecurityException;
-
 /**
- * Created by User on 01-12-2016.
+ * Upload & Local Storage Directory Configuration.
  */
-
 public class Config {
     // File upload url (points to local .NET Core REST API backend /api/image/upload)
     public static final String FILE_UPLOAD_URL = UrlConfig.FILE_UPLOAD_URL;
     public static final String FILE_UPLOAD_URL_UP = UrlConfig.FILE_UPLOAD_URL;
-    // Directory name to store captured images and videos
+
+    // Directory name to store captured images locally
     public static final String IMAGE_DIRECTORY_NAME = "Passenger";
-    public static String DOMAINURL1 = "https://fleetsmart3.ui.sensel.in";
-    public static final String FILE_UPLOAD_URL_VOLLEY = DOMAINURL1+"/SenselRestService.svc/rest/v3/";
-
-
 }
